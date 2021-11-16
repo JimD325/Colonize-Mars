44432a097b3bd2d2474e827b15dd6166565becfb
Jim Doyle <jamesdoyle202@gmail.com>
2021-11-15 21:21:08 -0800

    Functions added to Mars

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
+'use strict'
 
 let test ="my test variable"
 
-let userName = prompt("What is your name?");
-let currentHour = prompt("What is the current hour of the day (1-24)");
-let bodyweight = prompt("what is your current weight (in pounds)?")
+function makeGreeting(){
+    let userName = getUserName();
+    let greeting = hourOfDay();
+    let weighing = bodyweight();
+    document.write(greeting + "," + userName.toUpperCase() + ", your weight on the red planet is " + weighing + " lbs");
+}
 
+function getUserName () {
+    let userName = prompt("What is your name?");
+    if(userName.toLowerCase() === "jim"){
+        alert("Welcome Back Commander");
+    } else{
+        alert("Welcome Back to Space Command");
+    }
+    return userName;
+}
 
+function hourOfDay(){
+    let currentHour = prompt("What is the current hour of the day (1-24)");
+    let greeting =""
 if(currentHour >= 17 && currentHour <= 24){
     greeting = "Good evening";
 } else if(currentHour >= 12 && currentHour <= 16){
     greeting = "Good afternoon";
-}else if(currentHour > 0) {
-    greeting = "Good morning";
 }else {
-    greeting = "You are not welcome"
+    greeting = "Good morning";
+}    
+
+return greeting; 
+
+}
+function bodyweight(){
+    let bodyweight = prompt("what is your current weight (in pounds)?")
+    bodyweight = bodyweight / 3
+    return bodyweight;
 }
 
-bodyweight = bodyweight / 3
-
-
-
-if(userName.toLowerCase() === "jim"){
-    alert("Welcome Back Commander");
-} else{
-    alert("Welcome Back to Space Command");
-}
-
-document.write(greeting + "," + userName.toUpperCase() + ", your weight on the red planet is " + bodyweight + " lbs")
